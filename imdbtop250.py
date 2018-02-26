bae486959d0271b62043e5dc5f525d3c753dbec7
natemwilson1 <36828625+natemwilson1@users.noreply.github.com>
2018-02-25 20:03:19 -0500

    Add files via upload

---
+++ imdbtop250.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import csv
+from collections import Counter
+from datetime import datetime
+from dateutil import parser
+import os
+
+source = requests.get('http://www.imdb.com/chart/top').text
+
+soup = BeautifulSoup(source, 'html.parser')
+results = soup.find_all('td',attrs={'class':'titleColumn'})
+
+listy = []
+for result in results:
+    number = result.contents[0][7:-8]
+    title = result.contents[1].text
+    year = result.find('span').text[1:-1]
+    listy.append((number, title, year))
+
+df = pd.DataFrame(listy, columns=['number','title','year'])
+df.to_csv('movielist.csv',index=False,encoding='utf-8')
+print(df)
+print(input("Press enter to close"))
+
+x = []
+
+with open('movielist.csv','r') as listfile:
+    plots = csv.reader(listfile, delimiter=',')
+    for row in plots:
+        x.append(row[2])
+
+
+x.remove('year')
+x2 = Counter(x)
+counted = sorted(x2.items())
+
+x,y = zip(*counted)
+
+x = list(map(int,x))
+
+sns.barplot(x,y)
+plt.xticks(rotation=70)
+plt.title('IMDB Top 250 by Year')
+plt.xlabel('year')
+plt.ylabel('number of movies in the top 250')
+plt.tick_params(axis='x', which='major', labelsize=6)
+plt.tight_layout()
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+plt.show()
+
+
+
+
